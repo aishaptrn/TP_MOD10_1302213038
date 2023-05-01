f8357ea74857ae988cdbd793a48ef341c6593148
Aisha Nuryan <91200696+aishaptrn@users.noreply.github.com>
2023-05-01 21:06:56 +0700

    Library Construction

---
+++ AljabarLibApp/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using AljabarLibraries;
+
+namespace AljabarLibApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // fungsi mencari akar kuadrat
+            Console.WriteLine("Test Fungsi Akar Kuadrat");
+            double[] akarKuadrat = Kuadrat.AkarPersamaanKuadrat(new double[] { 1, -3, -10 });
+            Console.WriteLine("Eqt\t: x^2 - 3x - 10 = 0");
+            Console.WriteLine("Output\t: {" + akarKuadrat[0] + ", " + akarKuadrat[1] + "}");
+
+            // fungsi mencari hasil kuadrat
+            Console.WriteLine("\nTest Fungsi Hasil Kuadrat");
+            double[] hasilKuadrat = Kuadrat.HasilKuadrat(new double[] { 2, -3 });
+            Console.WriteLine("Eqt\t: 2x - 3");
+            Console.WriteLine("Output\t: {" + hasilKuadrat[0] + ", " + hasilKuadrat[1] + ", " + hasilKuadrat[2] + "}");
+        }
+    }
+}
---
+++ AljabarLibraries/Kuadrat.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AljabarLibraries
+{
+    public class Kuadrat
+    {
+        public static double[] AkarPersamaanKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double c = persamaan[2];
+
+            double r = Math.Sqrt((b * b) - (4 * a * c));
+
+            double x1, x2;
+
+            if (r >= 0)
+            {
+                x1 = (-b + r) / (2 * a);
+                x2 = (-b - r) / (2 * a);
+
+                return new double[] { x1, x2 };
+            } 
+            else
+            {
+                return new double[] { };
+            }
+        }
+
+        public static double[] HasilKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+
+            double a2 = a * a;
+            double b2 = b * b;
+            double ab = 2 * a * b;
+
+            return new double[] { a2, ab, b2 };
+        }
+    }
+}
